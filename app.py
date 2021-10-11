e349a39c6f9f04b07161b6c29d7b36acaf31d4a9
jaskaran <karanstar2397@gmail.com>
2021-10-11 22:03:29 +0530

    Changes in main app

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
-# for testing purpose we will use sqlite database.
-#  for production we may shift to mysql database.
-
-from security import authenticate, identity
-from flask import Flask, request
-from flask_restful import Resource,Api,reqparse
+from flask import Flask 
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT,jwt_required
-from user import UserRegister
-from security import authenticate,identity
-from item import  Item,ItemList
+from utils.security import authenticate, identity
+from resources.User import RegisterUser, UserLogin
 
 app=Flask(__name__)
-app.secret_key='jose'
+app.secret_key='memorylane'
 api=Api(app)
 
-jwt=JWT(app,authenticate,identity) 
+api.add_resource(RegisterUser,'/register')
+
+jwt=JWT(app,authenticate,identity) # creates new end point... /auth
 
 
-# api.add_resource(Item,'/item/<string:name>')
-# api.add_resource(ItemList,'/items')
-api.add_resource(UserRegister,'/register')
 
 if __name__=='__main__':
     app.run(port=5000,debug=True)
