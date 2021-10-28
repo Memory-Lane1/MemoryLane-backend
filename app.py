6f9b9b3c7d8b92d749fae14ce402003035244dca
jaskaran <karanstar2397@gmail.com>
2021-10-28 17:47:28 +0530

    modified main app

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
 from flask import Flask 
 from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT,jwt_required
-from utils.security import authenticate, identity
-from resources.User import RegisterUser, UserLogin
+from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from resources.User import RegisterUser, UserLogin, RefreshLogin, UpdatePassword, ForgotPassword, Profile , UserLogout
+# import datetime
+from datetime import timedelta
+app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = "somesecretcode"
 
-app=Flask(__name__)
-app.secret_key='memorylane'
-api=Api(app)
+ACCESS_EXPIRES= timedelta(hours=1)
 
-api.add_resource(RegisterUser,'/register')
+app.config["JWT_ACCESS_TOKEN_EXPIRES"]=ACCESS_EXPIRES
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(minutes=1)
+app.config['JWT_BLACKLIST_ENABLED'] = True
+jwt = JWTManager(app)
 
-jwt=JWT(app,authenticate,identity) # creates new end point... /auth
+CORS(app)
+api = Api(app)
+
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header,jwt_payload):
+    jti = jwt_payload["jti"]
+    token_in_redis = blocklist.jwt_redis_blocklist.get(jti)
+    return token_in_redis is not None
 
 
+api.add_resource(RegisterUser, '/register')
+api.add_resource(UserLogin, '/login')
+api.add_resource(Profile,'/myprofile')
+api.add_resource(RefreshLogin, '/reauth')
+api.add_resource(UpdatePassword, '/password/change')
+api.add_resource(ForgotPassword,'/password/get_new')
+api.add_resource(UserLogout,'/logout')
 
-if __name__=='__main__':
-    app.run(port=5000,debug=True)
+if __name__ == '__main__':
+    app.run(debug = False)
