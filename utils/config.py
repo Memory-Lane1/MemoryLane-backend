41515c2ea44069605dd1f426787b3f677aa88c9a
jaskaran <karanstar2397@gmail.com>
2021-10-29 13:02:59 +0530

    Config file modified

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 import os
-email_password = os.environ.get("email_password")
+email_password ='DE^7Y3*PpN'
 api_key = os.environ.get("api_key")
 secret_key = os.environ.get("secret_key")
-db_url = os.environ.get("db_url")
-redis_url = os.environ.get("redis_url")
-redis_password = os.environ.get("redis_password")
+# db_url = os.environ.get("db_url")
+# redis_url = os.environ.get("redis_url")
+# redis_password = os.environ.get("redis_password")
